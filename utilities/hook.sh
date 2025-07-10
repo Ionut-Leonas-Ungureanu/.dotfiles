f60bff5022d03a31d6263add2ed62ac6c659c058
Ionut-Leonas Ungureanu <uionutleonas@gmail.com>
2025-07-10 22:08:33 +0300

    added markdown support in nvim, updated hooks

--- utilities/hook.sh
+++ utilities/hook.sh
@@ -34,13 +34,15 @@ interview() {
 }
 
 tankaboom() {
-	project_path="~/workspace/tankaboom/tankaboom.frontend/src/"
+	project_path="~/workspace/tankaboom"
+	frontend_path="$project_path/tankaboom.frontend/src"
 
 	tmux has-session -t $SESSION 2>/dev/null
 
 	if [ $? != 0 ]; then
-		tmux new-session -d -s $SESSION -n nvim "cd $project_path && nvim ."
-		tmux new-window -t $SESSION:1 -n testing "cd $project_path && python3 -m http.server 8000"
+		tmux new-session -d -s $SESSION -n frontend "cd $frontend_path && nvim ."
+		tmux new-window -t $SESSION:1 -n frontend-testing "cd $frontend_path && python serve.py"
+		tmux new-window -t $SESSION:2 -n README "cd $project_path && nvim README.md"
 		tmux select-window -t $SESSION:0
 	fi
 
